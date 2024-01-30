19fd1551db378c04add277e5b19f18f16cf91906
LeonidasJD <milandjordjevic633@gmail.com>
2024-01-30 21:09:04 +0100

    kreiranje player komponente

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,12 +1,34 @@
+import { useState } from "react";
+
 export default function Player({name,symbol}){
+    let [isEditing, setIsEditing] = useState(false);
+    let [buttonLabel,setButtonLabel] = useState("Edit");
+
+
+    function handleEditClick(){
+        if(isEditing){
+            setIsEditing(false);
+        }
+        else{
+            setIsEditing(true);
+        }
+
+        if(buttonLabel === "Edit"){
+            setButtonLabel("Save");
+        }else{
+            setButtonLabel("Edit");
+        }
+        console.log("hello");
+    }
     return(
         <li>
             <span className="player">
-            <span className="player-name">{name}</span>
+                {isEditing === false ? <span className="player-name">{name}</span> : <input type="text" required/>}
+            
             <span className="player-symbol">{symbol}</span>
-            <button>Edit</button>
+            <button onClick={()=>handleEditClick()}>{buttonLabel}</button>
             </span>
           </li>
     );
 
-}
+}  
