49f81e06f6df7725db1dfdab7312b8f5b80e7f78
LeonidasJD <milandjordjevic633@gmail.com>
2024-01-30 20:39:17 +0100

    dodati igraci u app komponentu

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,34 @@
+import Header from "./components/Header";
+import headerLogo from "/game-logo.png";
+import Player from "./components/Player";
 
 function App() {
   
 
   return (
-    <h1>React Tic-Tac-Toe</h1>
-  )
+    <>
+    <Header
+   text={"Tic-Tac-Toe"}
+    image={headerLogo} 
+    alternative={"Hand drawn tic tac toe logo"}/>
+    
+    <main>
+      <div id="game-container">
+        <ol id="players">
+        <Player name={"Milan"} symbol={"X"}/>
+        <Player name={"Sandra"} symbol={"O"}/>
+          
+        </ol>
+      </div>
+    </main>
+    </>
+      
+    
+   
+
+    
+  );
+  
 }
 
 export default App
