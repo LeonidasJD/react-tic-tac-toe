a5466825ace33f7c4a1f72ffbf0fe9cfb19cf642
LeonidasJD <milandjordjevic633@gmail.com>
2024-01-31 17:36:34 +0100

    izmene u player komponenti

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,32 +1,36 @@
 import { useState } from "react";
 
-export default function Player({name,symbol}){
+export default function Player({initialName,symbol}){
     let [isEditing, setIsEditing] = useState(false);
-    let [buttonLabel,setButtonLabel] = useState("Edit");
+    let [playerName,setPlayerName] = useState(initialName)
+    
 
 
-    function handleEditClick(){
-        if(isEditing){
+    function handleEditClick(){ //funkcija kojom postavljmo stanje na true ili false , ako je false prikazace se ime igraca ako je true prikazace se input gde upisujemo ime igraca
+       /** if(isEditing){
             setIsEditing(false);
         }
         else{
             setIsEditing(true);
-        }
+        } */
 
-        if(buttonLabel === "Edit"){
-            setButtonLabel("Save");
-        }else{
-            setButtonLabel("Edit");
-        }
-        console.log("hello");
+        setIsEditing(prevIsEditing => !prevIsEditing); //ovo je skracena verzija if else uslova koju sam kreirao iznad
+        
     }
+
+    function onHandleChange(event){
+        console.log(event.target.value);
+        setPlayerName(event.target.value);
+        //FUNKCIJA SE IZVRSAVA NA ONCHANGE I POSTAVLJA VREDNOST OBJEKTA (INPUT POLJA) KAO NOVO STANJE,A TO NOVO STANJE SE POSTAVLJA KAO IME IGRACA
+    }
+    
     return(
         <li>
             <span className="player">
-                {isEditing === false ? <span className="player-name">{name}</span> : <input type="text" required/>}
+                {!isEditing  ? <span className="player-name">{playerName}</span> : <input type="text" required value={playerName} onChange={onHandleChange}/>}
             
             <span className="player-symbol">{symbol}</span>
-            <button onClick={()=>handleEditClick()}>{buttonLabel}</button>
+            <button onClick={()=>handleEditClick()}>{!isEditing ? "Edit" : "Save"}</button>
             </span>
           </li>
     );
