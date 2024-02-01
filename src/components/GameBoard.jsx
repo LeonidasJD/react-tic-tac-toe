4aba8b3dd7882d62db34749a1ee568a706eb29b1
LeonidasJD <milandjordjevic633@gmail.com>
2024-02-01 14:46:00 +0100

    azuriranje stanja game boarda

--- src/components/GameBoard.jsx
+++ src/components/GameBoard.jsx
@@ -1,3 +1,5 @@
+import { useState } from "react";
+
 export default function GameBoard() {
   const initialGameBoard = [
     [null, null, null],
@@ -5,9 +7,22 @@ export default function GameBoard() {
     [null, null, null],
   ]; //KREIRAN NIZ KOJI U SEBI SADRZI TRI NIZA - MULTI-DIMENZIONALNI NIZ
 
+  let [gameBoard, setGameBoard] = useState(initialGameBoard);
+
+  function onHandleSelectSquare(rowIndex, colIndex) {
+    setGameBoard((prevGameBoard) => {
+      //U setGameBoard KAO TRENUTNO STANJE PROVERAM PRETHODNO STANJE BOARD-A
+      const updatedBoard = [
+        ...prevGameBoard.map((innerArray) => [...innerArray]), //U AZURIRANO STANJE BOARDA STAVLJAM KOPIJU STAROG STANJA(PROLAZIM KROZ NIZOVE BOARDA I PAKUJEM IH U NOVI NIZ innerArray), KOPIJA SE STAVLJA KAKO NE BI OBRISALI STARO STANJE I SAMO POSTAVILI NOVO
+      ];
+      updatedBoard[rowIndex][colIndex] = "X"; //U AZURIRANO STANJE POSTAVLJAM NOVO STANJE TAKO DA U NIZ U TACAN RED I KOLONU POSTAVLJAM X
+      return updatedBoard; //VRACAM AZURIRANO I STARO STANJE KAKO BI STARI ZNAKOVI OSTALI I DODALI SE NOVI
+    });
+  }
+
   return (
     <ol id="game-board">
-      {initialGameBoard.map(
+      {gameBoard.map(
         (
           row,
           rowIndex // PROLAZIM KROZ NIZ initialGameBoard  I OD NJEGA KREIRAM LIST ITEM KAKO BIH DOBIO 3 REDA OD TRI NIZA KOJI SU U initialGameBoard NIZU
@@ -20,7 +35,11 @@ export default function GameBoard() {
                   colIndex // UNUTAR LIST ITEMA PROLAZIM KROZ SVAKI POJEDINACNI NIZ row  JER JE ROW KREIRAN KROZ MAP . I TAKO UNUTAR SVAKOG LIST ITEM IMAM PO TRI LIST ITEMA UNUTAR
                 ) => (
                   <li key={colIndex}>
-                    <button>{playerSymbol}</button>
+                    <button
+                      onClick={() => onHandleSelectSquare(rowIndex, colIndex)}
+                    >
+                      {playerSymbol}
+                    </button>
                   </li>
                 )
               )}
