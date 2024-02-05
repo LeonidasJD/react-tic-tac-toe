de3bc3b8bcef638d3885b4362feb1e3e05eb5ad3
LeonidasJD <milandjordjevic633@gmail.com>
2024-02-05 17:13:42 +0100

    ubacivanje logike iz gameBoard komponente

--- src/App.jsx
+++ src/App.jsx
@@ -3,15 +3,48 @@ import headerLogo from "/game-logo.png";
 import Player from "./components/Player";
 import GameBoard from "./components/GameBoard";
 import Log from "./components/Log";
+import { WINNING_COMBINATIONS } from "./wining-combinations";
 import { useState } from "react";
 
 function App() {
   let [gameTurns,setGameTurns] = useState([]); //STANJE KOJE PRIKAZUJE PODATKE O TOME KOJI JE ZNAK NA POTEZU I KOJI DEO GAMEBORDA SMO POPUNULI  
   let [activePlayer, setActivePlayer] = useState("X");//stanje koje prikazuje  koji znak je sledeci za igru da li je sledeci klik X ili O
+  
+
+  const initialGameBoard = [
+    [null, null, null],
+    [null, null, null],
+    [null, null, null],
+  ]; //KREIRAN NIZ KOJI U SEBI SADRZI TRI NIZA - MULTI-DIMENZIONALNI NIZ KOJI PREDSTAVLJA BOARD ZA IGRU
+
+   let gameBoard = initialGameBoard;
+
+   for (const turn of gameTurns){ //PROLAZIMIO KROZ NIZ gameTurns KOJI JE ZAPRAVO STATE KOJI PRIKAZUJE PODATKE O SVAKOM KLIKU NA NEKO DUGME NA TABLI
+    const {square,player} = turn;// KORISTIMO DESCTRUCTURING  ASSIGNMENT KAKO BI IZVUKLI VREDNOST SQUARE I PLAYER IZ OBJEKTA TURN
+    const {row,col} = square; //KORISTIMO DESCTRUCTURING  ASSIGNMENT KAKO BI IZVUKLI VREDNOST ROW I COL IZ OBJEKTA SQUARE
+
+    gameBoard [row][col] = player;// UNUTAR GAMEBOARD 2D NIZA U ODGOVAARAJUCU KOLONU I RED POSTAVLJAMO ODREDJEN ZNAK X ILI O KOJI JE DEFINISAN U OBJEKTU TURN U APP KOMPONENTI
+   }
+
+   let winner = null;
+   for (const combination of WINNING_COMBINATIONS){//   PROLAZIMO KROZ NIZ KOMBINACIJA KOORDINATA POLJA KOJA CE OZNACITI DA IMA POBEDNIKA
+    const firstSquareSymbol = gameBoard[combination[0].row][combination[0].column];
+    const secondSquareSymbol = gameBoard[combination[1].row][combination[1].column]; //KREIRAMO TRI POLJA GDE ZA SVAKO PROVERAVAMO UNUTAR NASEG GAME BOARDA KOJE JE VREDNOSTI DA LI X ILI O
+    const thirdSquareSymbol = gameBoard[combination[2].row][combination[2].column];
+    
+    if(firstSquareSymbol && firstSquareSymbol == secondSquareSymbol && firstSquareSymbol == thirdSquareSymbol){
+      console.log("winer is" , firstSquareSymbol); //AKO POSTOJI PRVI ZNAK I AKO SU SVI ZNAKOVI ISTI ONDA ZNACI DA IMAMO POBEDNIKA
+      winner = firstSquareSymbol;
+       
+    }
+   }
 
   function onHandleSelectPlayer(rowIndex,colIndex) {
+
+   
+
     setActivePlayer((currentActivePlayer) =>
-      currentActivePlayer === "X" ? "O" : "X"
+      currentActivePlayer === "X" ? "O" : "X"//postavlja stanje activePlayer na X ili O koje koristimo za stilizovanje playera da li ce biti uokviren borderom ili ne 
     );
     
     setGameTurns((prevTurns)=>{ //KREIRAMO NOVO STANJE , GDE UZIUMAMO PRETHODNO STANJE TABLE ZA IGRU I PROVERAMO AKO JE TRENUTNO IGRAO X ,SLEDECI ZNAK JESTE 0
@@ -19,6 +52,8 @@ function App() {
 
       if(prevTurns.length > 0 && prevTurns[0].player==="X"){
           currentPlayer = "O";
+      }else {
+        currentPlayer = "X"
       }
 
       const updatedTurns = [{square:{row:rowIndex,col:colIndex},player:currentPlayer},...prevTurns] // KAO NOVO STANJE POSTAVLJAMO NIZ  OD PRETHODNOG STANJE PLUS NOVO ,GDE NAM JE SQUARE ZAPRAVO 
@@ -50,9 +85,11 @@ function App() {
               isActive={activePlayer === "O"}
             />
           </ol>
+          {winner !==null ? <p>Winner is player {winner}</p> : null}
           <GameBoard
             onSelectSquare={onHandleSelectPlayer}
-            turns={gameTurns}
+            board={gameBoard}
+            
           />
         </div>
         <Log turnsLogs={gameTurns}/>
