628dcb6e4e9e21a60e71772037459b392cd1eb84
LeonidasJD <milandjordjevic633@gmail.com>
2024-02-06 16:27:08 +0100

    uni

--- src/components/GameOver.jsx
+++ src/components/GameOver.jsx
@@ -3,7 +3,6 @@ export default function GameOver({ winner }) {
     <div id="game-over">
       <h2>Game Over!</h2>
       {winner ? <p>{winner} won!</p> : <p>It is a draw!</p>}
-
       <p>
         <button>Rematch!</button>
       </p>
