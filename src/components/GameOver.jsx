402598ff5de5cdd7cff076faa417de6375f27a03
LeonidasJD <milandjordjevic633@gmail.com>
2024-02-06 16:37:26 +0100

    postavljen event listener

--- src/components/GameOver.jsx
+++ src/components/GameOver.jsx
@@ -1,10 +1,10 @@
-export default function GameOver({ winner }) {
+export default function GameOver({ winner, isRematch }) {
   return (
     <div id="game-over">
       <h2>Game Over!</h2>
       {winner ? <p>{winner} won!</p> : <p>It is a draw!</p>}
       <p>
-        <button>Rematch!</button>
+        <button onClick={isRematch}>Rematch!</button>
       </p>
     </div>
   );
